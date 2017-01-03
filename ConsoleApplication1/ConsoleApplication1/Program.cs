ae2f2be7f54b2ead0123363e2ae934c15d4c98bc
Joshua Barnett <jbarnett808@hotmail.com>
2017-01-03 15:09:00 -0800

    Comments

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,6 +8,10 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        /// <summary>
+        /// Add Comments
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hi Y'all!");
