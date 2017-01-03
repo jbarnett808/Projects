78cd3c92a6e2bb509ed8950b94c8da2b8435c8ed
Joshua Barnett <jbarnett808@hotmail.com>
2017-01-03 15:09:44 -0800

    Revert "Localize in en_US"
    This reverts commit f7991b57d32e95a19d22bcbb85d640d0fa24c428.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApplication1
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Y'all!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
